e3bfb3307bd457f85a0b2d0c044443bfdc2552d1
Andrey Vorobyov <vorandrew@gmail.com>
2016-08-01 12:22:35 +0300

    v_position

--- v_pos.sql
+++ v_pos.sql
@@ -1,8 +1,33 @@
 CREATE OR REPLACE VIEW v_pos AS
- SELECT
-   trades.symbol,
-    sum(trades.qua) AS qua,
-    sum(trades.qua * trades.price) / sum(trades.qua) AS price
+WITH pnl as (
+  SELECT
+    id,
+    SUM(qua) OVER s pos,
+    CASE
+      WHEN (SUM(qua) OVER s)::NUMERIC / qua < 0 THEN qua * (lag(price) OVER s - price)
+      ELSE 0
+    END pnl
   FROM trades
-  GROUP BY trades.symbol
- HAVING sum(trades.qua) <> 0;
+  WINDOW s AS (PARTITION BY symbol ORDER BY dt)
+), calc AS (
+    SELECT
+      symbol,
+      t.id,
+      dt,
+      SUM(qua) OVER s qua,
+      SUM(qua * price + pnl) OVER s / SUM(qua) OVER s price
+    FROM
+      trades t
+      LEFT JOIN pnl ON t.id = pnl.id
+    WHERE
+      pnl.pos != 0
+    WINDOW s AS ( PARTITION BY symbol ORDER BY dt )
+)
+SELECT
+  DISTINCT ON (symbol)
+  symbol,
+  qua,
+  price
+FROM
+  calc
+ORDER BY symbol, dt DESC
